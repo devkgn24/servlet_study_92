4c3904618c043faa2987ea66e63d5844f5b5874b
devkgn24 <devkgn24@gmail.com>
2025-06-25 15:45:55 +0900

    [김가남] 쿠키 생성, 수정, 사용, 삭제

--- lec04_session/src/main/java/com/gn/controller/EditCookieServlet.java
+++ lec04_session/src/main/java/com/gn/controller/EditCookieServlet.java
@@ -1,31 +1,33 @@
 package com.gn.controller;
 
+import java.io.IOException;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
-@WebServlet("/makeCookie")
-public class MakeCookieServlet extends HttpServlet {
+
+@WebServlet("/editCookie")
+public class EditCookieServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    public MakeCookieServlet() {
+    public EditCookieServlet() {
         super();
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// 1. 쿠키 객체 생성
-		// key가 중복되면 value를 덮어씀 -> key값 이름 정할때 조심!!
-		Cookie c = new Cookie("user_id","user01");
-		// 2. 쿠키 유지 시간 설정(초 단위)
-		c.setMaxAge(60 * 60 * 24); // 1일 유지
-		// 3. 응답에 쿠키 추가하여 브라우저에 전달
-		response.addCookie(c);
+		// user_id 라는 name을 가진 쿠키의 value를 admin으로 수정
+		Cookie cookie = new Cookie("user_id","user02");
+		// 기존과 동일한 유지 시간 설정
+		cookie.setMaxAge(60 * 60 * 24);
+		response.addCookie(cookie);
+		// => index.jsp화면에서 <쿠키 수정> 클릭후 새로고침했을 때 변경된 아이디 화면 노출
+		// 브라우저가 새로운 요청을 다시 보내게 하는 메소드 : sendRedirect
+		response.sendRedirect("/");
 	}
-
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
--- lec04_session/src/main/webapp/index.jsp
+++ lec04_session/src/main/webapp/index.jsp
@@ -12,22 +12,30 @@
 		<li>
 			<a href="/makeCookie">쿠키 생성</a>
 		</li>
+		<li>
+			<a href="/editCookie">쿠키 수정</a>
+		</li>
+		<li>
+			<a href="/deleteCookie">쿠키 삭제</a>
+		</li>
 	</ul>
 	<h2>현재 쿠키 값 확인</h2>
 	<%
 		// 1. 클라이언트가 보낸 쿠키 배열 가져오기
 		Cookie[] cookies = request.getCookies();
+		String userId = "쿠키 없음";
 	
 		// 2. 쿠키 배열에서 원하는 쿠키 찾기
 		if(cookies != null){
 			for(Cookie c : cookies){
 				if(c.getName().equals("user_id")){
-					String userId = c.getValue();
+					userId = c.getValue();
+					break;
 				}
 			}
 		}
 	%>
-	
+	<p>user_id 쿠키값 : <%=userId %></p>
 	
 	
 	
