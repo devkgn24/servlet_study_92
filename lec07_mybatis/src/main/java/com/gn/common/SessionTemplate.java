c3f5f27b75b8c0a4cc7cb712efaa25a15230e92b
devkgn24 <devkgn24@gmail.com>
2025-06-30 15:50:59 +0900

    [김가남] 목록 조회

--- lec07_mybatis/src/main/java/com/gn/common/SessionTemplate.java
+++ lec07_mybatis/src/main/java/com/gn/common/SessionTemplate.java
@@ -1,4 +1,4 @@
-package lec07_mybatis;
+package com.gn.common;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -9,8 +9,8 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 public class SessionTemplate {
-	public static SqlSession getSqlSession() {
-		
+	public static SqlSession getSqlSession(boolean autoCommit) {
+		SqlSession session = null;
 		try {
 			// 1. SqlSessionFactoryBuilder
 			// mybatis-config.xml의 설정 정보를 통해 객체 생성
@@ -27,11 +27,12 @@ public class SessionTemplate {
 			
 			// 3. SqlSession 객체 생성
 			// SqlSessionFactory의 openSession() 메소드 사용
-			// 
-			
-			
+			// true : autoCommit 켜기 -> 테이터 변경 자동 반영
+			// false : 끄기(명시적으로 commit 호출) -> 수동
+			session = factory.openSession(autoCommit);
 		} catch(IOException e) {
 			e.printStackTrace();
 		}
+		return session;
 	}
 }
--- lec07_mybatis/src/main/webapp/index.jsp
+++ lec07_mybatis/src/main/webapp/index.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="jakarta.tags.core" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +8,13 @@
 <title>마이바티스</title>
 </head>
 <body>
-
+	<!-- 1. index.jsp에 목록조회 a태그 생성 
+		 2. 목록조회 요청을 받는 Servlet(Controller)
+		 -->
+	<ol>
+		<li>
+			<a href="<c:url value='/student/list'/>">학생 목록 조회</a>
+		</li>
+	</ol>
 </body>
 </html>
